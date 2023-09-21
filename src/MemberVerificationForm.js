0705d61459beeda0bd601adcb8a2e90bea219eeb
Kavya <Kavya.Athuluri@emids.com>
2023-09-21 19:45:18 +0530

    updated UI

--- src/MemberVerificationForm.js
+++ src/MemberVerificationForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './MemberVerificationForm.css'; // Import your CSS file
 
 function MemberVerificationForm() {
   const [formData, setFormData] = useState({
@@ -27,47 +28,52 @@ function MemberVerificationForm() {
     <div>
       <h2>Member Verification Form</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>Member ID:</label>
+        <div className="form-group">
+          <label htmlFor="memberId">Member ID:</label>
           <input
             type="text"
             name="memberId"
+            id="memberId"
             value={formData.memberId}
             onChange={handleChange}
           />
         </div>
-        <div>
-          <label>Group ID:</label>
+        <div className="form-group">
+          <label htmlFor="groupId">Group ID:</label>
           <input
             type="text"
             name="groupId"
+            id="groupId"
             value={formData.groupId}
             onChange={handleChange}
           />
         </div>
-        <div>
-          <label>First Name:</label>
+        <div className="form-group">
+          <label htmlFor="firstName">First Name:</label>
           <input
             type="text"
             name="firstName"
+            id="firstName"
             value={formData.firstName}
             onChange={handleChange}
           />
         </div>
-        <div>
-          <label>Last Name:</label>
+        <div className="form-group">
+          <label htmlFor="lastName">Last Name:</label>
           <input
             type="text"
             name="lastName"
+            id="lastName"
             value={formData.lastName}
             onChange={handleChange}
           />
         </div>
-        <div>
-          <label>Date of Birth:</label>
+        <div className="form-group">
+          <label htmlFor="dateOfBirth">Date of Birth:</label>
           <input
             type="date"
             name="dateOfBirth"
+            id="dateOfBirth"
             value={formData.dateOfBirth}
             onChange={handleChange}
           />
