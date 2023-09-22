ee1b7c46bdb9c150b432bbb64aa86c4ffc0258bb
kavyaathuluri <kavyaa@emids.com>
2023-09-22 13:32:36 +0530

    Update MemberVerificationForm.js

--- src/MemberVerificationForm.js
+++ src/MemberVerificationForm.js
@@ -46,6 +46,7 @@ function MemberVerificationForm() {
   return (
     <div>
       <h2>Member Verification Form</h2>
+    <Link to="/admin">Go to Admin Page</Link>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="memberId">Member ID:</label>
