055de497351f6cd6ef9edc15122afe02c78f70d0
kavyaathuluri <kavyaa@emids.com>
2023-09-22 17:24:15 +0530

    Update AdminInterface.js

--- src/AdminInterface.js
+++ src/AdminInterface.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 //import * as microsoftTeams from "@microsoft/teams-js";
 import './AdminInterface.css'; 
-//import axios from 'axios';
+import axios from 'axios';
 
 
 const AdminInterface = () => {
@@ -44,6 +44,36 @@ console.info(data);
   const approveMember = (memberId) => {
     // Implement logic to approve the member with memberId.
     // You may make an API call to update the approval status.
+  };
+  const approveMember = (memberId) => {
+    const axios = require('axios');
+    //https://teams.microsoft.com/l/channel/19%3ab5e569d7055b4dc8a0a40ff5e150a7b2%40thread.tacv2/Member%2520Form?groupId=b0e35fe5-a737-4891-9a75-2b7e1e5ad92f&tenantId=6077507f-bcd4-4ca1-bebd-e4ac1d05ffa4
+                const accessToken = '7905c4b1-007e-4b5a-869a-18d12932d312'; // Replace with your actual access token
+                const channelId = 'b5e569d7055b4dc8a0a40ff5e150a7b2';//19:b5e569d7055b4dc8a0a40ff5e150a7b2@thread.tacv2/Member%20Form?groupId=b0e35fe5-a737-4891-9a75-2b7e1e5ad92f&tenantId=6077507f-bcd4-4ca1-bebd-e4ac1d05ffa4
+                // Replace with the channel's ID
+    
+                const message = {
+                body: {
+                    content: 'Authorization is successful!',
+                },
+                };
+    
+                const url = `https://graph.microsoft.com/v1.0/teams/${channelId}/messages`;
+    
+                const headers = {
+                Authorization: `Bearer ${accessToken}`,
+                'Content-Type': 'application/json',
+                };
+    
+                axios.post(url, message, { headers })
+                .then((response) => {
+                    console.log('Message sent:', response.data);
+                })
+                .catch((error) => {
+                    console.error('Error sending message:', error);
+                });
+    
+            }
   };
  /* const approveMember = (memberId) => {
         if (window.microsoftTeams) {
